dd3f6f050e025e472e982267b5d0b88183f60008
ivanmontoya7 <80126920+ivanmontoya7@users.noreply.github.com>
2024-05-29 12:55:46 +0200

    Corrección y optimización
    Ahora coge la ruta del vídeo correctamente.
    La comprobación de las combinaciones de la tabla ha sido optimizada, reduciendo los ifs.

--- js/03juego.js
+++ js/03juego.js
@@ -9,8 +9,6 @@ class juego {
         this.x = 0
 
         this.video = document.getElementById("recompensa");
-
-
         //Declara las casillas cogiendo el id de cada una
         //TODO:habría que meter las casillas en matriz
         for (let i = 0; i < this.rows; i++) {
@@ -80,47 +78,70 @@ class juego {
     }
 
     reel() {
-        this.eleccion_video=Math.floor(Math.random()*4);
-        this.ruta_video = this.es_premio()[0].src[74];
-        console.log(this.eleccion_video);
-        this.video.src = "./vids/" + this.ruta_video + "/"+this.eleccion_video+".mp4";
+        this.eleccion_video = Math.floor(Math.random() * 4);
+        this.ruta_video = this.es_premio()[0].src;
 
-/*         let error = false;
-        while (error) {
-            console.log("Error al cargar el video");
-            this.eleccion_video = Math.floor(Math.random() * 11);
-            this.video.src = "file:///C:/Users/MONTOYA/Desktop/Programacion/vsc_extension_gambling/vids/" + this.ruta_video + "/" + this.eleccion_video + ".mp4";
-            console.log(error);
-            error = false; // Set error to false to exit the loop
-        } */
-        
+        this.video.src = "./vids/" + this.ruta_video[this.ruta_video.length-5] + "/" + this.eleccion_video + ".mp4";
+
+        /*         let error = false;
+                while (error) {
+                    console.log("Error al cargar el video");
+                    this.eleccion_video = Math.floor(Math.random() * 11);
+                    this.video.src = "file:///C:/Users/MONTOYA/Desktop/Programacion/vsc_extension_gambling/vids/" + this.ruta_video + "/" + this.eleccion_video + ".mp4";
+                    console.log(error);
+                    error = false; // Set error to false to exit the loop
+                } */
+
         this.mostrar_contenido(this.video);
         this.video.autoplay = true;
         this.video.play();
-        
+
 
     }
 
     es_premio() {
         //TODO: hacer combinaciones con la matriz al declarar las casillas, o crear matrices con combinaciones ganadoras
-        switch (true) {
-            case (this.casilla1.src === this.casilla2.src && this.casilla2.src === this.casilla3.src):
-                return [this.casilla1, this.casilla2, this.casilla3];
-            case (this.casilla4.src === this.casilla5.src && this.casilla5.src === this.casilla6.src):
-                return [this.casilla4, this.casilla5, this.casilla6];
-            case (this.casilla7.src === this.casilla8.src && this.casilla8.src === this.casilla9.src):
-                return [this.casilla7, this.casilla8, this.casilla9];
-            case (this.casilla1.src == this.casilla4.src && this.casilla4.src == this.casilla7.src):
-                return [this.casilla1, this.casilla4, this.casilla7];
-            case (this.casilla2.src == this.casilla5.src && this.casilla5.src == this.casilla8.src):
-                return [this.casilla2, this.casilla5, this.casilla8];
-            case (this.casilla3.src == this.casilla6.src && this.casilla6.src == this.casilla9.src):
-                return [this.casilla3, this.casilla6, this.casilla9];
-            case (this.casilla1.src == this.casilla5.src && this.casilla5.src == this.casilla9.src):
-                return [this.casilla1, this.casilla5, this.casilla9];
-            default:
-                return 0;
+        
+        this.combinaciones = [
+            [this.casilla1, this.casilla2, this.casilla3],
+            [this.casilla4, this.casilla5, this.casilla6],
+            [this.casilla7, this.casilla8, this.casilla9],
+            [this.casilla1, this.casilla4, this.casilla7],
+            [this.casilla2, this.casilla5, this.casilla8],
+            [this.casilla3, this.casilla6, this.casilla9],
+            [this.casilla1, this.casilla5, this.casilla9],
+            [this.casilla7, this.casilla5, this.casilla3]
+        ];
+
+        for (let combinacion of this.combinaciones) {
+            if (combinacion[0].src === combinacion[1].src && combinacion[1].src === combinacion[2].src) {
+            return [combinacion[0], combinacion[1], combinacion[2]];
+            }
         }
+
+        return false;
+
+
+
+        /* Funciona, pero es poco optimo */
+        /*         switch (true) {
+                    case (this.casilla1.src === this.casilla2.src && this.casilla2.src === this.casilla3.src):
+                        return [this.casilla1, this.casilla2, this.casilla3];
+                    case (this.casilla4.src === this.casilla5.src && this.casilla5.src === this.casilla6.src):
+                        return [this.casilla4, this.casilla5, this.casilla6];
+                    case (this.casilla7.src === this.casilla8.src && this.casilla8.src === this.casilla9.src):
+                        return [this.casilla7, this.casilla8, this.casilla9];
+                    case (this.casilla1.src == this.casilla4.src && this.casilla4.src == this.casilla7.src):
+                        return [this.casilla1, this.casilla4, this.casilla7];
+                    case (this.casilla2.src == this.casilla5.src && this.casilla5.src == this.casilla8.src):
+                        return [this.casilla2, this.casilla5, this.casilla8];
+                    case (this.casilla3.src == this.casilla6.src && this.casilla6.src == this.casilla9.src):
+                        return [this.casilla3, this.casilla6, this.casilla9];
+                    case (this.casilla1.src == this.casilla5.src && this.casilla5.src == this.casilla9.src):
+                        return [this.casilla1, this.casilla5, this.casilla9];
+                    default:
+                        return 0;
+                } */
 
     }
 
